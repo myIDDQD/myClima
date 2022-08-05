ca84e596652553628689e1ab1d3b5e647a1d788d
Mykola <nickkosh@gmail.com>
2022-08-05 16:17:57 +0200

    start VC

---
+++ myClima/Controller/WeatherViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  myClima
+//
+//  Created by Mykola on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: Any) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+}
+
