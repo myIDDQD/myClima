9e0ad2edd58f72e81ae53c35add7d3a701009e20
Mykola <nickkosh@gmail.com>
2022-08-08 21:05:03 +0200

    add URLsession

--- myClima/Model/WeatherManager.swift
+++ myClima/Model/WeatherManager.swift
@@ -1,4 +1,6 @@
 
+import Foundation
+
 struct WeatherManager {
     
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=af647f4d92b20f6d9991e96ab6122fa7&units=metric&lang=ua"
@@ -6,5 +8,34 @@ struct WeatherManager {
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print(urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // create URL
+        
+        if let url = URL(string: urlString) {
+            // create URLsession
+            let session = URLSession(configuration: .default)
+            
+            // give the session task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            // start task
+            task.resume()
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
     }
 }
